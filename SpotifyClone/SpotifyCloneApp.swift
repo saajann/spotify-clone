e62a6b1c84a139475f7a6332a996800145f5d486
Saajan Saini <sainisaaji@gmail.com>
2026-05-15 15:59:20 +0200

    Initial Commit

---
+++ SpotifyClone/SpotifyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCloneApp.swift
+//  SpotifyClone
+//
+//  Created by Saajan Saini on 15/05/26.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
